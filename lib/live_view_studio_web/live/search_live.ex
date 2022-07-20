7295e4b6bcab7dbeb46209b17b2d83b28a527aed
JuliaMathias <juliamss@gmail.com>
2022-07-20 14:50:00 -0300

    Version 0.16 Update

--- lib/live_view_studio_web/live/search_live.ex
+++ lib/live_view_studio_web/live/search_live.ex
@@ -19,10 +19,10 @@ defmodule LiveViewStudioWeb.SearchLive do
     <h1>Find a Store</h1>
     <div id="search">
       <form id="zip-search" phx-submit="zip-search">
-        <input type="text" name="zip" value="<%= @zip %>"
-               placeholder="Zip Code"
-               autofocus autocomplete="off"
-               <%= if @loading, do: "readonly" %> />
+        <input type="text" name="zip" value={@zip}
+          placeholder="Zip Code"
+          autofocus autocomplete="off"
+          readonly={@loading} />
 
         <button type="submit">
           <img src="images/search.svg">
