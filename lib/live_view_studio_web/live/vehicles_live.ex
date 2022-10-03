ed114ef7eee2c1156484d8f526e0d5229eee0d8d
JuliaMathias <juliamss@gmail.com>
2022-10-03 20:10:19 -0300

    Exercise: Practice Sorting

--- lib/live_view_studio_web/live/vehicles_live.ex
+++ lib/live_view_studio_web/live/vehicles_live.ex
@@ -12,12 +12,17 @@ defmodule LiveViewStudioWeb.VehiclesLive do
     page = String.to_integer(params["page"] || "1")
     per_page = String.to_integer(params["per_page"] || "5")
 
+    sort_by = (params["sort_by"] || "id") |> String.to_atom()
+    sort_order = (params["sort_order"] || "asc") |> String.to_atom()
+
     paginate_options = %{page: page, per_page: per_page}
-    vehicles = Vehicles.list_vehicles(paginate: paginate_options)
+    sort_options = %{sort_by: sort_by, sort_order: sort_order}
+
+    vehicles = Vehicles.list_vehicles(paginate: paginate_options, sort: sort_options)
 
     socket =
       assign(socket,
-        options: paginate_options,
+        options: Map.merge(paginate_options, sort_options),
         vehicles: vehicles
       )
 
@@ -34,7 +39,9 @@ defmodule LiveViewStudioWeb.VehiclesLive do
             socket,
             __MODULE__,
             page: socket.assigns.options.page,
-            per_page: per_page
+            per_page: per_page,
+            sort_by: socket.assigns.options.sort_by,
+            sort_order: socket.assigns.options.sort_order
           )
       )
 
@@ -57,16 +64,16 @@ defmodule LiveViewStudioWeb.VehiclesLive do
           <thead>
             <tr>
               <th>
-                ID
+                <%= sort_link(@socket, "ID", :id, @options) %>
               </th>
               <th>
-                Make
+                <%= sort_link(@socket, "Make", :make, @options) %>
               </th>
               <th>
-                Model
+                <%= sort_link(@socket, "Model", :model, @options) %>
               </th>
               <th>
-                Color
+                <%= sort_link(@socket, "Color", :color, @options) %>
               </th>
             </tr>
           </thead>
@@ -95,7 +102,7 @@ defmodule LiveViewStudioWeb.VehiclesLive do
           <%= pagination_link(@socket,
                               "Previous",
                               @options.page - 1,
-                              @options.per_page,
+                              @options,
                               "previous") %>
         <% end %>
         <%= for i <- (@options.page - 2)..(@options.page + 2), i > 0 do %>
@@ -103,7 +110,7 @@ defmodule LiveViewStudioWeb.VehiclesLive do
             <%= pagination_link(@socket,
                                   i,
                                   i,
-                                  @options.per_page,
+                                  @options,
                                   (if i == @options.page, do: "active")) %>
           <% end %>
         <% end %>
@@ -111,7 +118,7 @@ defmodule LiveViewStudioWeb.VehiclesLive do
           <%= pagination_link(@socket,
                               "Next",
                               @options.page + 1,
-                              @options.per_page,
+                              @options,
                               "next") %>
         <% end %>
       </div>
@@ -121,16 +128,45 @@ defmodule LiveViewStudioWeb.VehiclesLive do
     """
   end
 
-  defp pagination_link(socket, text, page, per_page, class) do
+  defp pagination_link(socket, text, page, options, class) do
     live_patch(text,
       to:
         Routes.live_path(
           socket,
           __MODULE__,
           page: page,
-          per_page: per_page
+          per_page: options.per_page,
+          sort_by: options.sort_by,
+          sort_order: options.sort_order
         ),
       class: class
     )
   end
+
+  defp sort_link(socket, text, sort_by, options) do
+    text =
+      if sort_by == options.sort_by do
+        text <> emoji(options.sort_order)
+      else
+        text
+      end
+
+    live_patch(text,
+      to:
+        Routes.live_path(
+          socket,
+          __MODULE__,
+          sort_by: sort_by,
+          sort_order: toggle_sort_order(options.sort_order),
+          page: options.page,
+          per_page: options.per_page
+        )
+    )
+  end
+
+  defp toggle_sort_order(:asc), do: :desc
+  defp toggle_sort_order(:desc), do: :asc
+
+  defp emoji(:asc), do: " ⬇"
+  defp emoji(:desc), do: " ⬆"
 end
