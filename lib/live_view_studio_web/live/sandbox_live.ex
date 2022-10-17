d4eb6ddad506b21bad2230d893042bab25ffd513
JuliaMathias <juliamss@gmail.com>
2022-10-17 17:21:12 -0300

    Exercise: Design Another Stateful Component

--- lib/live_view_studio_web/live/sandbox_live.ex
+++ lib/live_view_studio_web/live/sandbox_live.ex
@@ -1,11 +1,12 @@
 defmodule LiveViewStudioWeb.SandboxLive do
   use LiveViewStudioWeb, :live_view
 
+  alias LiveViewStudioWeb.DeliveryChargeComponent
   alias LiveViewStudioWeb.QuoteComponent
   alias LiveViewStudioWeb.SandboxCalculatorComponent
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, weight: nil, price: nil)}
+    {:ok, assign(socket, weight: nil, price: nil, charge: 0)}
   end
 
   def render(assigns) do
@@ -13,15 +14,15 @@ defmodule LiveViewStudioWeb.SandboxLive do
     <h1>Build A Sandbox</h1>
 
     <div id="sandbox">
-      <%= live_component @socket, SandboxCalculatorComponent,
-                         id: 1,
-                         coupon: 10.0 %>
+      <%= live_component @socket, SandboxCalculatorComponent, id: 1, coupon: 10.0 %>
 
       <%= if @weight do %>
+        <%= live_component @socket, DeliveryChargeComponent, id: 2 %>
         <%= live_component @socket, QuoteComponent,
                           material: "sand",
                           weight: @weight,
-                          price: @price %>
+                          price: @price,
+                          charge: @charge %>
       <% end %>
     </div>
     """
@@ -29,6 +30,11 @@ defmodule LiveViewStudioWeb.SandboxLive do
 
   def handle_info({:totals, weight, price}, socket) do
     socket = assign(socket, weight: weight, price: price)
+    {:noreply, socket}
+  end
+
+  def handle_info({:charge, charge}, socket) do
+    socket = assign(socket, charge: charge)
     {:noreply, socket}
   end
 end
